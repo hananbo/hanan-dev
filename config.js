75302992ae97146eed03bc6e746c1eac0035c73e
Hanan Benold <hanan@rapidapi.com>
2019-06-08 21:45:59 +0300

    disabeling disqus

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ module.exports = {
   url: 'https://hanan.dev',
   title: 'Software Development Blog By Hanan Benold',
   subtitle: 'Where I write about software development and the culture around it', // description meta tag
-  disqusShortname: 'hanan-dev',
+  disqusShortname: '',
+  // disqusShortname: 'hanan-dev',
   postsPerPage: 5,
   googleAnalyticsId: 'UA-141671633-1',
   author: {
