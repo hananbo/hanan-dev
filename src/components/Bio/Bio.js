3b304a2160742c8c37dc3209ba78f5cad911758f
hanan <hananwm@gmail.com>
2020-01-04 22:31:19 +0200

    wpf launch

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -3,7 +3,11 @@ import React from 'react';
 import styles from '../Sidebar/Author/Author.module.scss';
 
 const Bio = () => (
-  <p className={styles['author__subtitle']}>Geek, Full Stack Developer, Team Lead. Currently working on building the world's largest API marketplace at <a href="https://rapidapi.com">RapidAPI</a></p>
+  <p className={styles['author__subtitle']}>Father, Husband, Geek, Full Stack Developer, Team Lead. Currently working on making it easier to show real estate for rent at <a href="https://showdigs.com" target="_blank">Showdigs</a>
+      <p>
+          On my free time, building & maintaining <a href="https://willpayfor.com" target="_blank">willpayfor.com</a> - a platform and community for sharing ideas worth paying for
+      </p>
+  </p>
 );
 
 export default Bio;
