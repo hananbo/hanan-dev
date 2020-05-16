792c7abe708832b3aeeaaa068bababca17eaa8a5
hanan <hanan@showdigs.com>
2020-05-16 11:53:12 +0300

    updating bio

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -4,10 +4,7 @@ import styles from '../Sidebar/Author/Author.module.scss';
 
 const Bio = () => (
   <p className={styles['author__subtitle']}>
-      Father, Husband, Geek, Full Stack Developer, Team Lead. Currently working on making it easier to show real estate for rent at <a href="https://showdigs.com" target="_blank">Showdigs</a>
-      <br/>
-      <br/>
-      On my free time, building & maintaining <a href="https://willpayfor.com" target="_blank">willpayfor.com</a> - a platform and community for sharing ideas worth paying for
+      Father, Husband, Geek, Full Stack Developer, Team Lead. Currently working on making it easier to show real estate for rent at <a href="https://showdigs.com" rel="noopener" target="_blank">Showdigs</a>
   </p>
 );
 
